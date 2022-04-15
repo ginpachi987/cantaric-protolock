20a4e58df5cfe66be91def38244a10eca2184120
ginpachi987 <ginpachi987@gmail.com>
2022-04-15 17:53:53 +0300

    v1.0 NC

--- src/main.js
+++ src/main.js
@@ -1,174 +1,38 @@
-/// <reference types="../node_modules/@types/p5/global" />
-
 import './style.css'
-import * as p5 from '../node_modules/p5/lib/p5'
 
 import offImage from './img/off.png'
 import onImage from './img/on.png'
-
-const sketch = window
-
-let cellSize
-let fieldDimension = 10
-let circleSize = 30
-let field
-let fieldSize
-let maxFieldSize = 600
-
-let selectedContoller, cX, cY
-
-let off, on
-
-let container
-
-sketch.preload = () => {
-  off = loadImage(offImage)
-  on = loadImage(onImage)
-}
-
-sketch.setup = () => {
-  container = document.querySelector('main')
-  recalcSizes()
-  createCanvas(fieldSize, fieldSize)
-
-  rectMode(CENTER)
-  imageMode(CENTER)
-  textAlign(CENTER)
-
-  // let resetB = document.querySelector('#reset')
-  // resetB.addEventListener('click', resetter)
-  // resetB.addEventListener('touchstart', resetter)
-
-  let newGame = document.querySelector('#new')
-  newGame.addEventListener('click', newField)
-  newGame.addEventListener('touchstart', newField)
-
-  field = new Field(fieldDimension)
-  field.newField()
-}
-
-sketch.draw = () => {
-  background(41)
-  push()
-  translate(cellSize / 2, cellSize / 2)
-  field.draw()
-  pop()
-}
-
-sketch.windowResized = () => {
-  recalcSizes()
-  resizeCanvas(fieldSize, fieldSize);
-  //redraw()
-}
-
-sketch.mouseMoved = () => {
-  let mX = mouseX //- (windowWidth - fieldSize) / 2
-  let mY = mouseY //- (windowHeight - fieldSize) / 2
-
-  if (mX < 0 || mX > fieldSize || mY < 0 || mY > fieldSize) {
-    selectedContoller = null
-    // container.style.cursor = 'default'
-    return
-  }
-
-  cX = Math.floor(mX / cellSize)
-  cY = Math.floor(mY / cellSize)
-
-
-  if (cX > 0 && cY < fieldDimension || (cX == 0 && cY == fieldDimension))
-    selectedContoller = null
-  else {
-    if (cX === 0) {
-      selectedContoller = field.leftSwitches[cY]
-    }
-    else {
-      selectedContoller = field.bottomSwitches[cX - 1]
-    }
-    // container.style.cursor = 'pointer'
-  }
-}
-
-sketch.mouseClicked = () => {
-  if (!selectedContoller) return
-
-  if (cX === 0) {
-    for (let i = 0; i < fieldDimension; i++) {
-      setTimeout((x, y) => {
-        field.cells[x][y].state = !field.cells[x][y].state
-      }, i * 50, i, cY)
-    }
-  }
-  else {
-    for (let i = 0; i < fieldDimension; i++) {
-      setTimeout((x, y) => {
-        field.cells[x][y].state = !field.cells[x][y].state
-      }, (fieldDimension - 1 - i) * 50, cX - 1, i)
-    }
-  }
-
-  winCheck()
-}
-
-function recalcSizes() {
-  fieldSize = Math.min(maxFieldSize, windowWidth, windowHeight) - 60
-  cellSize = fieldSize / (fieldDimension + 1)
-  circleSize = cellSize / 1.8
-
-  container.style.top = (windowHeight - fieldSize) / 2 + 'px'
-  container.style.left = (windowWidth - fieldSize) / 2 + 'px'
-}
-
-function newField() {
-  if (field) field.newField()
-}
-
-function winCheck() {
-  let notYet = false
-  for (let i = 0; i < fieldDimension; i++) {
-    for (let j = 0; j < fieldDimension; j++) {
-      if (field.cells[i][j].state == true) {
-        notYet = true
-        break
-      }
-    }
-    if (notYet) break
-  }
-
-  if (!notYet) {
-    setTimeout(() => {
-      alert('Yay!')
-      field.newField()
-    }, 1000)
-  }
-}
-
-// function resetter() {}
+import switchImage from './img/switch.png'
 
 class Cell {
   constructor(state = false) {
     this.state = state
-    this.opacity = state ? 255 : 0
+    this.el = document.createElement('div')
+    this.el.classList.add('cell')
+    this.el.style.backgroundImage = `url('${offImage}')`
   }
 
-  draw(x, y) {
-    if (this.state && this.opacity < 255) {
-      this.opacity += 85
-    }
-    else if (!this.state && this.opacity > 0) {
-      this.opacity -= 85
-    }
-    push()
-    image(off, x * cellSize, y * cellSize, cellSize, cellSize)
-    tint(255, this.opacity)
-    image(on, x * cellSize, y * cellSize, cellSize, cellSize)
-    pop()
+  toggleState(state) {
+    this.state = state || !this.state
+    this.el.style.backgroundImage = `url('${this.state ? onImage : offImage}')`
   }
 }
 
 class Switch {
-  constructor(x, y) {
-    this.x = x
-    this.y = y
+  constructor(field, x, y) {
+    this.field = field
+    this.el = document.createElement('div')
+    this.el.classList.add('switch')
+    if (x != null || y != null) {
+      this.image = document.createElement('img')
+      this.image.classList.add('switch-image')
+      this.image.src = switchImage
+
+      this.image.addEventListener('click', () => {
+        field.toggleCells(x, y)
+      })
+      this.el.appendChild(this.image)
+    }
   }
 
   draw(angle) {
@@ -207,12 +71,48 @@ class Switch {
 class Field {
   constructor(size) {
     this.cells = [...Array(size)].map(() => [...Array(size).keys()].map(() => new Cell()))
-    this.controlAngle = 0
-    this.leftSwitches = [...Array(size).keys()].map((_, i) => new Switch(0, i))
-    this.bottomSwitches = [...Array(size).keys()].map((_, i) => new Switch((i + 1), size))
+    this.leftSwitches = [...Array(size).keys()].map((_, i) => new Switch(this, i, null))
+    this.bottomSwitches = [new Switch(this, null, null)].concat([...Array(size).keys()].map((_, i) => new Switch(this, null, i)))
 
-    this.display = false
-    this.transparency = 0
+    for (let i = 0; i <= size; i++) {
+      let row = document.createElement('div')
+      row.classList.add('row')
+      if (i < size) {
+        row.appendChild(this.leftSwitches[i].el)
+        for (let j = 0; j < size; j++) {
+          row.appendChild(this.cells[i][j].el)
+        }
+      }
+      else {
+        for (let j = 0; j <= size; j++) {
+          row.appendChild(this.bottomSwitches[j].el)
+        }
+      }
+
+      container.appendChild(row)
+    }
+  }
+
+  toggleCells(row, col) {
+    if (row == null) {
+      for (let i = 0; i < fieldDimension; i++) {
+        setTimeout((x, y) => {
+          field.cells[x][y].toggleState()
+        }, (fieldDimension - 1 - i) * 50, i, col)
+      }
+    }
+    else {
+      for (let i = 0; i < fieldDimension; i++) {
+        setTimeout((x, y) => {
+          field.cells[x][y].toggleState()
+        }, i * 50, row, i)
+      }
+    }
+
+    setTimeout(() => {
+      winCheck()
+    }, fieldDimension * 50)
+
   }
 
   newField() {
@@ -220,13 +120,13 @@ class Field {
     let rands = this.getRandomNumbers(rand ? 4 : 3)
     for (let i = 0; i < (rand ? 4 : 3); i++) {
       for (let j = 0; j < fieldDimension; j++) {
-        this.cells[rands[i]][j].state = !this.cells[rands[i]][j].state
+        this.cells[rands[i]][j].toggleState()
       }
     }
     rands = this.getRandomNumbers(!rand ? 4 : 3)
     for (let i = 0; i < (!rand ? 4 : 3); i++) {
       for (let j = 0; j < fieldDimension; j++) {
-        this.cells[j][rands[i]].state = !this.cells[j][rands[i]].state
+        this.cells[j][rands[i]].toggleState()
       }
     }
   }
@@ -234,7 +134,7 @@ class Field {
   getRandomNumbers(amount) {
     let arr = []
     while (arr.length < amount) {
-      let el = floor(Math.random() * fieldDimension)
+      let el = Math.floor(Math.random() * fieldDimension)
       if (arr.indexOf(el) === -1) arr.push(el)
     }
     return arr
@@ -255,4 +155,93 @@ class Field {
     this.controlAngle -= .01
     if (this.controlAngle < -PI * 2) this.controlAngle = 0
   }
+
+  setSize(size) {
+    this.cells.forEach((row, i) => {
+      row.forEach((cell, j) => {
+        cell.el.style.width = `${size}px`
+        cell.el.style.height = `${size}px`
+        cell.el.style.backgroundSize = `${size}px`
+      })
+    })
+    this.bottomSwitches.forEach((control, i) => {
+      control.el.style.width = `${size}px`
+      control.el.style.height = `${size}px`
+    })
+    this.leftSwitches.forEach((control, i) => {
+      control.el.style.width = `${size}px`
+      control.el.style.height = `${size}px`
+    })
+  }
 }
+
+// let cellSize
+let fieldDimension = 10
+let circleSize = 30
+let field
+let fieldSize
+let maxFieldSize = 600
+
+let selectedContoller, cX, cY
+
+let off, on
+
+let container
+
+setup()
+
+function setup() {
+  container = document.querySelector('#game')
+
+  // let resetB = document.querySelector('#reset')
+  // resetB.addEventListener('click', resetter)
+  // resetB.addEventListener('touchstart', resetter)
+
+  let newGame = document.querySelector('#new')
+  newGame.addEventListener('click', newField)
+  newGame.addEventListener('touchstart', newField)
+
+  field = new Field(fieldDimension)
+  field.newField()
+
+  window.addEventListener('resize', recalcSizes)
+
+  recalcSizes()
+}
+
+function recalcSizes() {
+  fieldSize = Math.min(maxFieldSize, window.innerWidth, window.innerHeight) - 60
+  let cellSize = fieldSize / (fieldDimension + 1)
+  // circleSize = cellSize / 1.8
+
+  container.style.width = `${fieldSize}px`
+
+  if (field) field.setSize(cellSize)
+  //container.style.height = `${fieldSize}px`
+}
+
+function newField() {
+  if (field) field.newField()
+}
+
+function winCheck() {
+  let notYet = false
+  for (let i = 0; i < fieldDimension; i++) {
+    for (let j = 0; j < fieldDimension; j++) {
+      if (field.cells[i][j].state == true) {
+        notYet = true
+        break
+      }
+    }
+    if (notYet) break
+  }
+
+  if (!notYet) {
+    setTimeout(() => {
+      alert('Yay!')
+      field.newField()
+    }, 1000)
+  }
+}
+
+// function resetter() {}
